ffce8d71547c383555dc6577289a971b6e0b4659
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-06-12 10:37:43 -0500

    Updated wifi reset script

--- terminal/wifi-reset.sh
+++ terminal/wifi-reset.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 # This assume the device is named 3d:00.0. Do an lspci -k to find the actual name
-echo 1 | sudo tee /sys/bus/pci/devices/0000\:3d\:00.0/remove
+echo 1 >| /sys/bus/pci/devices/0000\:3d\:00.0/remove
 sleep 3
-echo 1 | sudo tee /sys/bus/pci
-
+echo 1 >| /sys/bus/pci/rescan
